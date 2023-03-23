a6dcb8245df84f14f2ef433ba029d71e8530b983
gacharyzoniea <zgoniea@gmail.com>
2023-03-23 19:04:30 -0400

    Movement basic Done

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerMovementScript : MonoBehaviour
 {
     Vector3 _movement;
-    Vector3 _gravity = new Vector3(0, -.5f, 0);
-    CharacterController _characterController;
-    float _speed = .5f;
+    Vector3 _currentMovement;
+
+    private float verticalDirection;
+    Rigidbody rbody;
     public SlapFighter playerMovement; //input object
-    [SerializeField] private float acceleration = 10f;
-    [SerializeField] private float deceleration = 10f;
-    [SerializeField] private float smoothingFactor = 0.1f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    private bool _changingDirection => (rbody.velocity.x > 0f && _movement.x < 0f) || (rbody.velocity.x < 0f && _movement.x > 0f);
+
+    [Header ("Movement")]
+    [SerializeField] float _speed = 6.24f;
+    [SerializeField] private float _groundLinearDrag = 2.65f;
+    [SerializeField] private float _airLinearDrag = 1f;
+    [SerializeField] private float maxSpeed = 10;
+    [SerializeField] private float jumpSpeed = 9f;
+    [SerializeField] float gravity = 10f;
+
+    [Header("GroundCheck")]
+    [SerializeField] float sphereCastRadius = .1f;
+    [SerializeField] float sphereCastDistance = .2f;
+
+    [Header ("Jumping")]
+    [SerializeField] private float _fallMultiplier = 8f;
+    [SerializeField] private float _jumpVelocityFalloff = 8;
 
     private InputAction _move;
     private InputAction _jump;
@@ -26,39 +45,104 @@ public class PlayerMovementScript : MonoBehaviour
     private void OnEnable()
     {
         _move = playerMovement.Player.Move;
+        _jump = playerMovement.Player.Jump;
+        _jump.Enable();
         _move.Enable();
     }
 
     private void OnDisable()
     {
+        _jump.Disable();
         _move.Disable();
     }
+
     void Start()
     {
-        _characterController = GetComponent<CharacterController>();
+        rbody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        Vector3 currentVelocity = _characterController.velocity;
-        Vector3 newVelocity = Vector3.Lerp(currentVelocity, _movement, smoothingFactor);
-
-        if (Mathf.Approximately(_movement.x, 0f))
+        MoveCharacter();
+        if (isGrounded)
         {
-            newVelocity.x = Mathf.Lerp(currentVelocity.x, _movement.x, deceleration * Time.fixedDeltaTime);
+            ApplyGroundLinearDrag();
         }
         else
         {
-            newVelocity.x = Mathf.Lerp(currentVelocity.x, _movement.x, acceleration * Time.fixedDeltaTime);
+            ApplyAirLinearDrag();
+            FallMultiplier();
         }
-
-        _characterController.Move(newVelocity * _speed);
-        _characterController.Move(_gravity);
     }
+
+    
 
     private void Update()
     {
+        verticalDirection = rbody.velocity.y;
+        _currentMovement = rbody.velocity;
+        //check if grounded
+        isGrounded = Physics.SphereCast(transform.position, sphereCastRadius, Vector3.down, out RaycastHit hitInfo, sphereCastDistance, groundLayer);
+        print(isGrounded);
+
+        //set movement vector w/ input system
         _movement = _move.ReadValue<Vector2>();
+        
+        //jump logic
+        if (_jump.triggered && isGrounded)
+        {
+            Jump();
+        }
+
+        //clamp max speed to avoid infinite acceleration
+        //if(rbody.velocity.x > maxSpeed)
+        //{
+        //    rbody.velocity = Vector3.ClampMagnitude(rbody.velocity, maxSpeed);
+        //}
+
+        rbody.AddForce(Vector3.down * gravity);
+    }
+    private void MoveCharacter()
+    {
+        rbody.AddForce(_movement * _speed, ForceMode.VelocityChange);
+        if (Mathf.Abs(rbody.velocity.x) > maxSpeed)
+            rbody.velocity = new Vector2(Mathf.Sign(rbody.velocity.x) * maxSpeed, rbody.velocity.y);
+        
     }
 
+    private void Jump()
+    {
+        rbody.velocity = Vector3.up * jumpSpeed;
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if (Mathf.Abs(_movement.x) < 0.4f || _changingDirection)
+        {
+            rbody.drag = _groundLinearDrag;
+        }
+        else
+        {
+            rbody.drag = 0f;
+        }
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+        rbody.drag = _airLinearDrag;
+    }
+
+    private void FallMultiplier()
+    {
+        if (rbody.velocity.y < _jumpVelocityFalloff)
+        {
+            rbody.velocity += _fallMultiplier * Physics.gravity.y * Vector3.up * Time.deltaTime;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = new Color32(0, 255, 0, 200);
+        Gizmos.DrawSphere(transform.position - new Vector3(0, sphereCastDistance, 0), sphereCastRadius);
+    }
 }
