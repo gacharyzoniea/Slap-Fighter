5de7709d147139f3578beb3131496614108130c3
TERZANODJ19 <terzanodj19@gcc.edu>
2023-04-14 16:26:31 -0400

    Go through platforms

--- Assets/Scripts/PlayerPlatformInteractScript.cs
+++ Assets/Scripts/PlayerPlatformInteractScript.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerPlatformInteractScript : MonoBehaviour
 {
 
-    public LayerMask platformLayer = 6;
+    //public LayerMask platformLayer = 6;
+    //public LayerMask groundLayer = 3;
+    int platformLayer = 6;
+    int groundLayer = 3;
+    int hitboxLayer = 12;
+    public Vector3 playerPos;
+
     //private PlayerMovementFixed _pm;
     //private InputAction _move;
     // Start is called before the first frame update
@@ -19,14 +25,21 @@ public class PlayerPlatformInteractScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
-        {
-            Physics.IgnoreLayerCollision(this.gameObject.layer, platformLayer, true);
+        
 
-        }
-        else
-        {
-            Physics.IgnoreLayerCollision(this.gameObject.layer, platformLayer, false);
-        }
+            if (Input.GetKey(KeyCode.S))
+            {
+
+                print("S");
+            //Vector3.Slerp(playerPos, new Vector3(playerPos.x, playerPos.y - 2f, playerPos.z), 0.01f);
+            //(new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 0.5f, this.gameObject.transform.position.z));
+                Physics.IgnoreLayerCollision(12, platformLayer, true);
+
+            }
+            else
+            {
+                Physics.IgnoreLayerCollision(12, platformLayer, false);
+            }
+
     }
 }
