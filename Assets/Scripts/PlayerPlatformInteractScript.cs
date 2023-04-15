1288f42c43520e69525b66a2681de597c17333bd
TERZANODJ19 <terzanodj19@gcc.edu>
2023-04-14 23:40:41 -0400

    Platform fixes

--- Assets/Scripts/PlayerPlatformInteractScript.cs
+++ Assets/Scripts/PlayerPlatformInteractScript.cs
@@ -11,14 +11,16 @@ public class PlayerPlatformInteractScript : MonoBehaviour
     int groundLayer = 3;
     int hitboxLayer = 12;
     public Vector3 playerPos;
+    playerAttackScript _playerAttackScript;
 
     //private PlayerMovementFixed _pm;
     //private InputAction _move;
     // Start is called before the first frame update
     void Start()
     {
-       // _pm = GetComponent<PlayerMovementFixed>();
-       // _move = player.FindAction("Move");
+        // _pm = GetComponent<PlayerMovementFixed>();
+        // _move = player.FindAction("Move");
+        _playerAttackScript = GetComponent<playerAttackScript>();
 
     }
 
@@ -27,19 +29,26 @@ public class PlayerPlatformInteractScript : MonoBehaviour
     {
         
 
-            if (Input.GetKey(KeyCode.S))
+            if (Input.GetKeyUp(KeyCode.S) && !_playerAttackScript.moveLag)
             {
 
                 print("S");
             //Vector3.Slerp(playerPos, new Vector3(playerPos.x, playerPos.y - 2f, playerPos.z), 0.01f);
             //(new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 0.5f, this.gameObject.transform.position.z));
-                Physics.IgnoreLayerCollision(12, platformLayer, true);
+                
+                StartCoroutine(endLag(0.8f));
 
             }
-            else
-            {
-                Physics.IgnoreLayerCollision(12, platformLayer, false);
-            }
+            
 
+    }
+
+    IEnumerator endLag(float endlag)
+    {
+        Physics.IgnoreLayerCollision(12, platformLayer, true);
+        //_playerAttackScript.moveLag = true;
+        yield return new WaitForSeconds(endlag);
+        //_playerAttackScript.moveLag = false;
+        Physics.IgnoreLayerCollision(12, platformLayer, false);
     }
 }
