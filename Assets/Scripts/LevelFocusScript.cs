c344657d06b653463d180f4c7a9dea81fb55642c
TERZANODJ19 <terzanodj19@gcc.edu>
2023-05-13 18:49:08 -0400

    Start of CameraMoveScript

---
+++ Assets/Scripts/LevelFocusScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFocusScript : MonoBehaviour
+{
+
+    public float halfXBounds = 20f;
+    public float halfYBounds = 15f;
+    public float halfZBounds = 15f;
+
+    public Bounds focusBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = gameObject.transform.position;
+        Bounds bounds = new Bounds();
+        bounds.Encapsulate(new Vector3(position.x - halfXBounds,
+            position.y - halfYBounds, position.z - halfZBounds));
+        bounds.Encapsulate(new Vector3(position.x + halfXBounds,
+           position.y + halfYBounds, position.z + halfZBounds));
+        focusBounds = bounds;
+    }
+}
