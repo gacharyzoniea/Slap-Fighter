a38d142274a818f66d4dfcb50716a99ddba2c515
gacharyzoniea <zgoniea@gmail.com>
2023-05-13 17:16:53 -0400

    menu button on victory screen connected
    also plays victory sound

---
+++ Assets/Scripts/EnableMenuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EnableMenuScript : MonoBehaviour
+{
+    AudioSource m_AudioSource;
+    public AudioClip m_Clip;
+    public GameObject menu;
+    private void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(menu);
+        m_AudioSource.PlayOneShot(m_Clip, .7f);
+    }
+}
